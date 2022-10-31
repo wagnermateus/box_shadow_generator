40936bf69689a9a61b827f4c6a33910e48775e6c
wagnermateus <wagnermateus7@gmail.com>
2022-10-31 10:11:03 +0100

    created the  updateBoxShadowProps method

--- js/scripts.js
+++ js/scripts.js
@@ -10,20 +10,37 @@ class BoxShadowGenerator {
     spreadInputValue
   ) {
     this.horizontalInput = horizontalInput;
-    this.horizontalInputValue = horizontalInputValue; 
+    this.horizontalInputValue = horizontalInputValue;
     this.verticalInput = verticalInput;
     this.verticalInputValue = verticalInputValue;
-    this.blurInput = blurInput; 
+    this.blurInput = blurInput;
     this.blurInputValue = blurInputValue;
     this.spreadInput = spreadInput;
     this.spreadInputValue = spreadInputValue;
   }
 
-  initialize(){
-    this.horizontalInputValue.value = this.horizontalInput.value; 
-    this.verticalInputValue.value = this.verticalInput.value; 
-    this.blurInputValue.value = this.blurInput.value; 
-    this.spreadInputValue.value = this.spreadInput.value; 
+  initialize() {
+    this.horizontalInputValue.value = this.horizontalInput.value;
+    this.verticalInputValue.value = this.verticalInput.value;
+    this.blurInputValue.value = this.blurInput.value;
+    this.spreadInputValue.value = this.spreadInput.value;
+  }
+
+  updateBoxShadowProperties(type, value) {
+    switch (type) {
+      case "horizontal":
+        this.horizontalInputValue.value = value;
+        break;
+      case "vertical":
+        this.verticalInputValue.value = value;
+        break;
+      case "blur":
+        this.blurInputValue.value = value;
+        break;
+      case "spread":
+        this.spreadInputValue.value = value;
+        break;  
+    }
   }
 }
 
@@ -36,16 +53,42 @@ const blurInputValue = document.querySelector("#blur-input-value");
 const spreadInput = document.querySelector("#spread-input");
 const spreadInputValue = document.querySelector("#spread-input-value");
 
+const rule = document.querySelector("#rule");
+const webkitRule = document.querySelector("#webkit-rule");
+const mozRule = document.querySelector("#moz-rule");
+
+const previewer = document.querySelector("#previewer");
+
 
 const boxShadow = new BoxShadowGenerator(
-    horizontalInput,
-    horizontalInputValue,
-    verticalInput,
-    verticalInputValue,
-    blurInput,
-    blurInputValue,
-    spreadInput,
-    spreadInputValue
+  horizontalInput,
+  horizontalInputValue,
+  verticalInput,
+  verticalInputValue,
+  blurInput,
+  blurInputValue,
+  spreadInput,
+  spreadInputValue
 );
 
 boxShadow.initialize();
+
+horizontalInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  boxShadow.updateBoxShadowProperties("horizontal", value);
+});
+
+verticalInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  boxShadow.updateBoxShadowProperties("vertical", value);
+});
+
+blurInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  boxShadow.updateBoxShadowProperties("blur", value);
+});
+
+spreadInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  boxShadow.updateBoxShadowProperties("spread", value);
+});
